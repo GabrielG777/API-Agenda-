f1d7f490065aaf383ce092a6cf1b81bc8687a1a6
gaaaaaaabriel <gg405246@gmail.com>
2025-11-18 21:53:49 -0300

    Adicionando Cors para conseguir acessar de outros lugares

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 // index.js (na raiz do projeto 'Api/')
 const express = require('express');
 const sequelize = require('./mysql/database');
+const cors = require('cors'); 
 
-// Importa as rotas
 const empresaRoutes = require('./routes/empresaRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const servicoRoutes = require('./routes/servicoRoutes'); 
 const prestadorRoutes = require('./routes/prestadorRoutes');
 const agendamentoRoutes = require('./routes/agendamentoRoutes'); 
 
-
 const app = express();
 const port = 3000;
 
+app.use(cors()); // cors parar conseguir acessar a API de outro domínio
 app.use(express.json());
 
-// Sincroniza os modelos
 sequelize.sync()
   .then(() => {
     console.log('🔄 Modelos sincronizados com o banco de dados.');
 
-    // Define os prefixos da API
     app.use('/api/empresas', empresaRoutes);
     app.use('/api/usuarios', usuarioRoutes);
     app.use('/api/servicos', servicoRoutes); 
     app.use('/api/prestador', prestadorRoutes); 
     app.use('/api/agendamentos', agendamentoRoutes); 
 
-    // Inicia o servidor
     app.listen(port, () => {
       console.log(`🚀 Servidor rodando em http://localhost:${port}`);
     });
